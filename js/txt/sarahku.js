0fe0fc702904be4eb5d1c1a8864fd8e313f6f698
Ahmadln <65666454+Ahmadln@users.noreply.github.com>
2024-02-26 13:34:37 +0700

    Update sarahku.js

--- js/txt/sarahku.js
+++ js/txt/sarahku.js
@@ -1,26 +1,40 @@
-// untuk setiap hari
+const messages = {
+    Sunday: {
+        morning: "Selamat Pagi Cintaku, Semoga dipagi minggu ini kondisi kamu vit ya siap untuk beraktifitas untuk hari ini.",
+        afternoon: "Selamat Siang",
+        evening: "Selamat sore sayangku"
+    },
+    Monday: {
+        morning: "Pagi cintaku, Semoga dipagi hari ini kondisi kamu vit ya sayang. Jangan biarkan Senin merusak semangatmu, Bersamamu kita hadapi segala hal cintaku. I Love you sayangku❤️ ",
+        afternoon: "Siang sayangku, Terimakasih Selalu ada untuk ku sayang, aku juga akan selalu ada untuk kamu, di siang ini kamu tetap semagat yaa menjalani hari ini. I Love you Sarahku❤️",
+        evening: "Malam cintaku, Kamu kalau sudah sampe rmh bersih-bersih dulu ya, setelah bersih langsung makan yaa sayangku. I Love you Bidadariku❤️"
+    },
+    Tuesday: {
+        morning: "Pagi cintaku, Semoga dipagi hari ini kondisi kamu vit ya sayang. Jangan biarkan hari ini merusak semangatmu, Bersamamu kita hadapi segala hal cintaku. I Love you sayangku❤️ ",
+        afternoon: "Siang sayangku, Terimakasih Selalu ada untuk ku sayang, aku juga akan selalu ada untuk kamu, di siang ini kamu tetap semagat yaa menjalani hari ini. I Love you Sarahku❤️",
+        evening: "Malam cintaku, Kamu kalau sudah sampe rmh bersih-bersih dulu ya, setelah bersih langsung makan yaa sayangku. I Love you Bidadariku❤️"
+    },
+    Wednesday: {
+        morning: "Selamat Rabu Pagi, Semoga dipagi hari ini kondisi kamu vit ya siap untuk beraktifitas untuk hari ini.",
+        afternoon: "Selamat Siang",
+        evening: "Selamat sore sayangku"
+    },
+    Thursday: {
+        morning: "Selamat Kamis Pagi, Semoga dipagi hari ini kondisi kamu vit ya siap untuk beraktifitas untuk hari ini.",
+        afternoon: "Selamat Siang",
+        evening: "Selamat sore sayangku"
+    },
+    Friday: {
+        morning: "Selamat Jumat Pagi, Semoga dipagi hari ini kondisi kamu vit ya siap untuk beraktifitas untuk hari ini.",
+        afternoon: "Selamat Siang",
+        evening: "Selamat sore sayangku"
+    },
+    Saturday: {
+        morning: "Selamat Sabtu Pagi, Semoga dipagi hari ini kondisi kamu vit ya siap untuk beraktifitas untuk hari ini.",
+        afternoon: "Selamat Siang",
+        evening: "Selamat sore sayangku"
+    },
+    Default: "Hello!"
+};
 
-export
-    const message = {
-        Monday: [
-            ["A very happy birthday, love. Senin"],
-        ],
-        Tuesday: [
-            ["A very happy birthday, love. Tuesday"],
-        ],
-        Wednesday: [
-            ["A very happy birthday, love. Wednesday"],
-        ],
-        Thursday: [
-            ["A very happy birthday, love. Thursday"],
-        ],
-        Friday: [
-            ["A very happy birthday, love. Jumat"],
-        ],
-        Saturday: [
-            ["A very happy birthday, love. Saturday"],
-        ],
-        default: 'ihhhh Hari ini bukan hari ulang tahun Kamu tau. tapi Terima Kasih sudah berkunjung! >_<',
-    };
-
-    console.log("sarahku bisa")
+export default messages;
