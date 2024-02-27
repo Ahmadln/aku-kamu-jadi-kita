ad697e3449bfeabc7044e4107a2b063842dae972
Ahmadln <65666454+Ahmadln@users.noreply.github.com>
2024-02-28 06:24:59 +0700

    Update rhlQhcq.js

--- js/txt/rhlQhcq.js
+++ js/txt/rhlQhcq.js
@@ -15,8 +15,8 @@ const messages = {
         evening: "Malam cintaku, dimalam hari ini sebelum kamu memulai aktifitas di rumah, kamu bersih-bersih duluu ya sayangku, setelah bersih langsung makan yaa sayangku, makannya jangan di atas jam 10 loh. Setelah itu kamu mau belajar kan, Tetap Semangatt yaa belajarnyaa walau ku tau pasti lelah seharian kamu balik kerja, tapi ingat untuk mencapai kesuksesan setidaknya setiap hari ada progress walau itu sedikit. I Love you Bidadariku❤️"
     },
     Wednesday: {
-        morning: "Selamat Rabu Pagi, Semoga dipagi hari ini kondisi kamu vit ya siap untuk beraktifitas untuk hari ini.",
-        afternoon: "Selamat Siang",
+        morning: "Selamat Pagii Ayangku, tau gasii disini hujan tauu, kamu disana hujan ga, bawa payung yaa sayng ku, kamu jngn lupa sarapan, dan semangatt ya untuk menjalani hari inii. kalau ada apaa² chat aku sayang, Akuu cintaa kamuu sarahku",
+        afternoon: "Selamat Siang cintaku, kamu istirahat siang bekalnya di habisin yaa, jngn disisain tauu mau nyisain buat siapa si, kalau buat aku mah boleh tp kan kita blm tentu ketemu hari ini, dan tetap semangattt sayangkuuu",
         evening: "Selamat sore sayangku"
     },
     Thursday: {
